47362f0d98535dab078fa298a9cd73c4c7cad2ff
wga22 <wga22@yahoo.com>
2017-04-02 10:28:39 -0400

    checking in automation files

---
+++ filework/updateCode.sh
@@ -0,0 +1,5 @@
+# TODO, setup and use env variable for NODEHOME
+wget -O /home/will/node/NHL/NHL_work.js https://github.com/wga22/nodejs/raw/master/nhl/NHL_work.js
+wget -O /home/will/node/tesla/will_tesla.js https://github.com/wga22/nodejs/raw/master/tesla/will_tesla.js
+wget -O /home/will/node/tesla/tesla_battery_levels.js https://github.com/wga22/nodejs/raw/master/tesla/tesla_battery_levels.js
+wget -O /home/will/node/vacuum/vacuum.js https://github.com/wga22/nodejs/raw/master/vacuum/vacuum.js
