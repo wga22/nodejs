0207baa7c4af279b4b934f341c6e72fe992a17be
Will <wga22@yahoo.com>
2017-09-26 11:17:28 -0400

    trimmed

--- nhl/misc/update-nhlpi.sh
+++ nhl/misc/update-nhlpi.sh
@@ -5,7 +5,6 @@
 
 (apt-get update && apt-get -y upgrade) > /dev/null
 apt-get dist-upgrade -y
-apt-get install -y raspi-gpio git build-essential python-dev python-pip python-imaging python-smbus libasound2-dev nodejs
 apt-get clean -y
 
 #pull git code
@@ -14,10 +13,5 @@ wget -O /opt/nhl/NHL_work.js https://github.com/wga22/nodejs/raw/master/nhl/NHL_
 wget -O /opt/nhl/index.html https://github.com/wga22/nodejs/raw/master/nhl/index.html
 wget -O /opt/nhl/webserver.js https://github.com/wga22/nodejs/raw/master/nhl/webserver.js
 wget -O /opt/nhl/nhl_common.js https://github.com/wga22/nodejs/raw/master/nhl/nhl_common.js
-
-
-# update environment variable with node_path
-## NOTE cp /etc/environment /etc/environment.bak
-## NOTE printf '\nexport NODE_PATH=/usr/local/lib/node_modules\n' >> /etc/environment
 
 exit 0
